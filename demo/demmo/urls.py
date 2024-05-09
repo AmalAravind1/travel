603305f07c21f5e4b458a8c4c3a350db3263079c
AmalAravind1 <155979228+AmalAravind1@users.noreply.github.com>
2024-05-09 12:59:25 +0530

    Update urls.py

--- demo/demmo/urls.py
+++ demo/demmo/urls.py
@@ -6,10 +6,5 @@ from . import views
 urlpatterns = [
 
     path('',views.index,name='index'),
-    # path('calc/',views.calc,name='calc')
-    # path('about/',views.about,name='about'),
-# path('contact/',views.contact,name='contact'),
-# path('details/',views.details,name='details'),
-# path('thanks/',views.thanks,name='thanks')
-
+    
 ]
